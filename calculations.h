4c3c135a062acfaef028f6118e7d8e38c0f61aad
ASNOLEN <164524830+ASNOLEN@users.noreply.github.com>
2024-10-28 18:41:36 -0400

    Update calculations.h
    update to work with its cpp file

--- calculations.h
+++ calculations.h
@@ -3,17 +3,18 @@
 //
 
 // calculations.h
-#ifndef CALCULATIONS_H
-#define CALCULATIONS_H
+#ifndef GYMNERD_CALCULATIONS_H
+#define GYMNERD_CALCULATIONS_H
 
+// Class for fitness calculations
 class Calculations {
 public:
     static double calculateBMI(double weight, double height);
     static double calculateBMR(double weight, double height, int age);
     static double calculateStrength(int bench, int squat, double weight);
     static double calculateVitality(int age, int waist, double height);
-    static double calculateAgility(double weight, double height);
-    static char calculateScore(double value);
+    static double calculateAgility(double weight, double height, double sprint);
+    static char calculateScore(double weight, double height, int bench, int squat, int sprint);
 };
 
-#endif //GYMNERD_CALCULATIONS_H
+#endif // GYMNERD_CALCULATIONS_H
