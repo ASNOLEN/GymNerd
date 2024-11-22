1276e6bcd33dce6cc1b63ef7c13a160400110fa3
ASNOLEN <164524830+ASNOLEN@users.noreply.github.com>
2024-11-22 09:44:08 -0500

    Final mainwindow.cpp
    Updated code to include the existing users function

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,37 +1,53 @@
-//the cpp of the main window of the program, lets user exit or make new user
-//libraries and headers
+//The main window of the program, lets user exit or make new user
+//Libraries and headers
 //------------------------------------------------------------
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "userinfo.h"
+#include "statscreen.h"
 #include "BackgroundWidget.h"
+#include "profilechooser.h"
 
-MainWindow::MainWindow(QWidget *parent)
-        : QMainWindow(parent), ui(new Ui::MainWindow), statsScreen(new statscreen), backgroundWidget(new BackgroundWidget(this)) {
-
+MainWindow::MainWindow(QWidget *parent) :
+        QMainWindow(parent),
+        ui(new Ui::MainWindow),
+        backgroundWidget(new BackgroundWidget(this)) //create the border image
+{
     ui->setupUi(this);
 
-    //make the border image fit on the screen and not obscure the buttons
+    //set border to fit window
     backgroundWidget->setGeometry(this->rect());
-    backgroundWidget->raise();
+    backgroundWidget->raise(); //make it go behind the buttons
 
-    //connect the buttons to a function/slot
-    connect(ui->nwuserbutton, &QPushButton::clicked, this, &MainWindow::openUserInfo);
+    //connect the buttons
+    connect(ui->nwuserbutton, &QPushButton::clicked, this, &MainWindow::onNewUserButtonClicked);
+    connect(ui->userButton, &QPushButton::clicked, this, &MainWindow::onExistingUserButtonClicked);
     connect(ui->exitprogbutton, &QPushButton::clicked, this, &MainWindow::close);
 }
 
-//make border size change dynamically
+MainWindow::~MainWindow() {
+    delete ui;
+}
+
 void MainWindow::resizeEvent(QResizeEvent *event) {
     QMainWindow::resizeEvent(event);
-    backgroundWidget->setGeometry(this->rect());
+    backgroundWidget->setGeometry(this->rect());  //set border to change with window
 }
 
-MainWindow::~MainWindow() {
-    delete ui;//delete the window
-}
-
-void MainWindow::openUserInfo() {//open the next window
+void MainWindow::onNewUserButtonClicked() {
     this->hide();
     userinfo *userInfo = new userinfo;
     userInfo->show();
+}
+//access the user file to get previous users
+void MainWindow::onExistingUserButtonClicked() {
+    ProfileChooser profileDialog(this);
+    profileDialog.loadData("prevuser.json");
+    //display previous user list
+    if (profileDialog.exec() == QDialog::Accepted) {
+        QJsonObject selectedUser = profileDialog.getSelectedUser();
+        statscreen* statsScreen = new statscreen();
+        statsScreen->loadData(selectedUser["name"].toString());
+        statsScreen->show();
+    }
 }
