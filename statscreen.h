de9dac9e4e147188c3f9a35b1d2c1c0aefbe79b4
ASNOLEN <164524830+ASNOLEN@users.noreply.github.com>
2024-11-22 09:53:42 -0500

    Final statscreen.h
    update to have a save feature and have a increase stat function for weekly challenges

--- statscreen.h
+++ statscreen.h
@@ -2,15 +2,14 @@
 #define GYMNERDS_STATSCREEN_H
 
 #include <QWidget>
-#include <QLabel>
+#include <QJsonObject>
 #include "BackgroundWidget.h"
 
 namespace Ui {
     class statscreen;
 }
 
-class statscreen : public QWidget
-{
+class statscreen : public QWidget {
 Q_OBJECT
 
 public:
@@ -19,15 +18,27 @@ public:
 
     void setStats(const QString& name, float bmi, float bmr, int strength, int vitality, int agility);
     void setRank(const QString& rank);
-
-
+    void loadData(const QString &usernameToLoad);
 
 protected:
     void resizeEvent(QResizeEvent *event) override;
 
-private://making the instances for the window
+private slots:
+    void onChallengeClicked();
+    void onReturnToMainClicked();
+
+private:
     Ui::statscreen *ui;
     BackgroundWidget *backgroundWidget;
+
+    QString username;
+    QString grade;
+    int strength;
+    int vitality;
+    int agility;
+
+    void updateStats();
+    void saveData();
 };
 
-#endif //GYMNERDS_STATSCREEN_H
+#endif // GYMNERDS_STATSCREEN_H
